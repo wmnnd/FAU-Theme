46746ee3b66be6ae5bf0252ffabe564bc62384dc
Philipp <git@vanbittern.com>
2015-10-28 10:13:33 +0100

    Move #subnav to bottom of content container on small screens

--- js/scripts.js
+++ js/scripts.js
@@ -292,16 +292,19 @@ jQuery(document).ready(function($) {
 		var heroNavigation = $('.hero-navigation');
 		var header = $('#header');
 		var metaNav = $('#meta-nav');
+		var subNav = $('#subnav').parent();
 		if (width > 750 && !body.hasClass('responsive-large')) {
 			body.addClass('responsive-large');
 			heroNavigation.appendTo('#hero');
 			header.insertAfter('#meta');
 			metaNav.appendTo('#meta .container .pull-left');
+			subNav.prependTo('#content .row');
 		} else if (width <= 750 && body.hasClass('responsive-large')) {
 			body.removeClass('responsive-large');
 			heroNavigation.prependTo('#footer');
 			header.prependTo('body');
 			metaNav.appendTo('#off-canvas');
+			subNav.appendTo('#content .row');
 		}
 	};
 
